269038eceddfb56ad525d2d9160bae303652d080
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-24 17:05:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function happyHolidays(){
   
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name){
+  
+  return `Happy holidays, ${name}!`
+  
+  }
+  
+  function happyHolidayTo (holiday, name){
+    
+    return `Happy ${holiday}, ${name}!`
+  }
+  
+  function holidayCountdown (holidayName, daysUntil){
+    return `It's ${days} until ${holiday}`
+    
+  }
