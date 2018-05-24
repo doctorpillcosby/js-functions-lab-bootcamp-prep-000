a75ce96663d8bbd0d381941d31f0c889ae3b87c6
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-24 17:13:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function happyHolidaysTo(name){
     return `Happy ${holiday}, ${name}!`
   }
   
-  function holidayCountdown (days, holiday){
+  function holidayCountdown (holiday, days){
     return `It's ${days} days until ${holiday}!
     `
     
